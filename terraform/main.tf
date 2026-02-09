8a9f5267a728fadfc03aae17820ae92d788f9be8
RusRares1337 <rusrares1997@gmail.com>
2026-02-09 19:30:56 +0200

    adding tf backend configuration

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+    required_version = ">=1.0"
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+# Local state
+backend "local" {
+    path = "terraform.tfstate"
+}
+
+provider "aws" {
+    region = var.aws_region
+    default_tags {
+        tags = {
+            Project     = "github-runners"
+            ManagedBy   = "terraform"
+            Environment = "var.environment"
+
+        }
+    }
+}
+
+
+}
